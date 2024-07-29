94477b5f1fd83db7bb89be0dd9956334b82d38ad
Joseph <jcheesebouroug2@ivytech.edu>
2024-07-28 22:53:39 -0400

    M05 Intro to Express

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+
+// register view engine
+app.set('view engine', 'ejs');
+
+
+app.listen(3000);
+
+
+app.get('/', (req, res) => {
+    const blogs = {
+        
+    }
+    res.render('index', { title: 'Home'});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About'});
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create New Blog'});
+});
+
+// 404 page
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404'});
+});    
